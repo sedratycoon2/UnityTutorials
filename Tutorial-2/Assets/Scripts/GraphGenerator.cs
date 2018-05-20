3a23b8d0786c793085956579b0a9a620084823d2
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 08:53:38 -0300

    Replaced while loop with for as it is best suited to iterate over ranges.

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -8,12 +8,10 @@ public class GraphGenerator : MonoBehaviour {
 
     private void Awake()
     {
-        int i = 0;
-        while (i < 10)
+        for (int i=0; i < 10; i++)
         {
             Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
             graphPoint.localPosition = Vector3.right * i; //sets the position of the clone to (i,0,0)
-            i = i + 1;
         }
         Destroy(GameObject.Find("Cube")); // destroys the original gameObject
     }
