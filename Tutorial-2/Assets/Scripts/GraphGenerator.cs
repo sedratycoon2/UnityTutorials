aecc7c5b4ecb68ad5531c60fc8fbc89add2121e5
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 12:55:14 -0300

    The cubes are in the range (-1,1) instead of [-1,1]

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -11,7 +11,7 @@ public class GraphGenerator : MonoBehaviour {
         for (int i=0; i < 10; i++)
         {
             Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
-            graphPoint.localPosition = Vector3.right * (i + 0.5f / 5f - 1f); // sets the position of the clone to (i,0,0) in the range [-1,1]
+            graphPoint.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f); // sets the position of the clone to (i,0,0) in the range [-1,1]
             graphPoint.localScale = Vector3.one / 5f; // reducing the scale of the clones to bring to [-1,1] domain
         }
         Destroy(GameObject.Find("Cube")); // destroys the original gameObject
