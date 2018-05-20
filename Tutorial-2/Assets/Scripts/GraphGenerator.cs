61d9b39da8fc19b14dd5219921b4f9aaec3b9217
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 08:47:59 -0300

    Added while loop to create ten instances of cube and set its position to (i,0,0)

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -8,11 +8,13 @@ public class GraphGenerator : MonoBehaviour {
 
     private void Awake()
     {
-        Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
-        graphPoint.localPosition = Vector3.right; //sets the position of the clone to (1,0,0)
-
-        graphPoint = Instantiate(graphPointPrefab); // creates another clone of cube
-        graphPoint.localPosition = Vector3.right * 2f; // sets the position of the clone to (2,0,0)
+        int i = 0;
+        while (i < 10)
+        {
+            i = i + 1;
+            Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
+            graphPoint.localPosition = Vector3.right * i; //sets the position of the clone to (i,0,0)
+        }
         Destroy(GameObject.Find("Cube")); // destroys the original gameObject
     }
 }
