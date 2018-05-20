49a4f1ea4378f775697b1e34492c3120d3fd1598
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 14:05:53 -0300

    Added graphResolution variable to adjust graph's resoultion (or graph's smoothness).

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class GraphGenerator : MonoBehaviour {
 
     public Transform graphPointPrefab;
+    // range to create more cubes, which can be adjusted via slider
+    [Range(10,100)]
+    public int graphResolution = 10;
 
     private void Awake()
     {
-        Vector3 graphScale = Vector3.one / 5f;
+        float graphStep = 2f / graphResolution; // used to calculate the scale
+        Vector3 graphScale = Vector3.one * graphStep;
         Vector3 graphPosition;
         graphPosition.z = 0;
-        for (int i=0; i < 10; i++)
+        for (int i=0; i < graphResolution; i++)
         {
             Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
-            graphPosition.x = (i + 0.5f) / 5f - 1f; // sets the x-position of the clone in the range (-1,1)
+            graphPosition.x = (i + 0.5f) / graphStep - 1f; // sets the x-position of the clone in the range (-1,1)
             graphPosition.y = Mathf.Pow(graphPosition.x, 2); // sets the y-position of the clone in the range (-1,1) 
             graphPoint.localPosition = graphPosition;
             graphPoint.localScale = graphScale; // reducing the scale of the clones to bring to (-1,1) domain
