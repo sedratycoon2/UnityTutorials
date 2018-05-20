f9ee485df3d2c6ae6ccb1d6de0dcd01de06d3f67
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 13:03:41 -0300

    Created a variable to store the scale instead of calulating at every iteration. Also, created a variable to store the position and set its y and z component to zero.

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -8,11 +8,16 @@ public class GraphGenerator : MonoBehaviour {
 
     private void Awake()
     {
+        Vector3 graphScale = Vector3.one / 5f;
+        Vector3 graphPosition;
+        graphPosition.y = 0;
+        graphPosition.z = 0;
         for (int i=0; i < 10; i++)
         {
             Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
-            graphPoint.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f); // sets the position of the clone to (i,0,0) in the range [-1,1]
-            graphPoint.localScale = Vector3.one / 5f; // reducing the scale of the clones to bring to [-1,1] domain
+            graphPosition.x = (i + 0.5f) / 5f - 1f; // sets the position of the clone to (i,0,0) in the range (-1,1)
+            graphPoint.localPosition = graphPosition;
+            graphPoint.localScale = graphScale; // reducing the scale of the clones to bring to (-1,1) domain
         }
         Destroy(GameObject.Find("Cube")); // destroys the original gameObject
     }
