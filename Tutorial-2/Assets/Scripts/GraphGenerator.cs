e4b26f7978d703f6766ab3ce6d26f5453342caa5
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 08:42:22 -0300

    Instantiated another clone and set its position to (2,0,0)

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -10,6 +10,9 @@ public class GraphGenerator : MonoBehaviour {
     {
         Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
         graphPoint.localPosition = Vector3.right; //sets the position of the clone to (1,0,0)
+
+        graphPoint = Instantiate(graphPointPrefab); // creates another clone of cube
+        graphPoint.localPosition = Vector3.right * 2f; // sets the position of the clone to (2,0,0)
         Destroy(GameObject.Find("Cube")); // destroys the original gameObject
     }
 }
