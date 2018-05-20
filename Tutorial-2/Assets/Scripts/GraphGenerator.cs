ad83a01f94b991653c15796c36d5d2999c369834
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 14:08:34 -0300

    Corrected the graphPosition.x calculations to reflect resolution.

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -14,11 +14,11 @@ public class GraphGenerator : MonoBehaviour {
         float graphStep = 2f / graphResolution; // used to calculate the scale
         Vector3 graphScale = Vector3.one * graphStep;
         Vector3 graphPosition;
-        graphPosition.z = 0;
+        graphPosition.z = 0f;
         for (int i=0; i < graphResolution; i++)
         {
             Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
-            graphPosition.x = (i + 0.5f) / graphStep - 1f; // sets the x-position of the clone in the range (-1,1)
+            graphPosition.x = (i + 0.5f) * graphStep - 1f; // sets the x-position of the clone in the range (-1,1)
             graphPosition.y = Mathf.Pow(graphPosition.x, 2); // sets the y-position of the clone in the range (-1,1) 
             graphPoint.localPosition = graphPosition;
             graphPoint.localScale = graphScale; // reducing the scale of the clones to bring to (-1,1) domain
