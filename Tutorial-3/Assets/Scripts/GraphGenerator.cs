4fcc4a6eba7b5795b1c0b6e819314c0734dca2b1
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 17:55:32 -0300

    Added Tutorial-3 with necessary pre-requisites.

---
+++ Tutorial-3/Assets/Scripts/GraphGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphGenerator : MonoBehaviour {
+
+    public Transform graphPointPrefab;
+    // range to create more cubes, which can be adjusted via slider
+    [Range(10,100)]
+    public int graphResolution = 10;
+    Transform [] graphPoints;
+
+    private void Awake()
+    {
+        float graphStep = 2f / graphResolution; // used to calculate the scale
+        Vector3 graphScale = Vector3.one * graphStep;
+        Vector3 graphPosition;
+        graphPosition.y = 0f;
+        graphPosition.z = 0f;
+        graphPoints = new Transform[graphResolution];
+        for (int i = 0; i < graphResolution; i++)
+        {
+            Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
+            graphPoints[i] = graphPoint; // storing the newly created clone's transform
+            graphPosition.x = (i + 0.5f) * graphStep - 1f; // sets the x-position of the clone in the range (-1,1)
+            graphPoint.localPosition = graphPosition;
+            graphPoint.localScale = graphScale; // reducing the scale of the clones to bring to (-1,1) domain
+            graphPoint.SetParent(transform, false);
+        }
+        Destroy(GameObject.Find("Cube")); // destroys the original gameObject
+    }
+
+    private void Update()
+    {
+        // updates graph point per frame
+        for (int i = 0; i < graphResolution; i++)
+        {
+            Transform newGraphPoint = graphPoints[i];
+            Vector3 newPointPos = newGraphPoint.localPosition;
+            newPointPos.y = Mathf.Sin(Mathf.PI * (newPointPos.x + Time.time)); // f(x) = sin(pi*x) type function
+            newGraphPoint.localPosition = newPointPos;
+        }
+    }
+}
