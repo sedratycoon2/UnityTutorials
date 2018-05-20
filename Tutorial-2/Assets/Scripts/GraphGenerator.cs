b664623fd381d928a7ccec2c7202b76f3353b379
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 12:45:39 -0300

    Changed the scale of the clones to 1/5th of the original w.r.t the placement.

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -11,7 +11,8 @@ public class GraphGenerator : MonoBehaviour {
         for (int i=0; i < 10; i++)
         {
             Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
-            graphPoint.localPosition = Vector3.right * i; //sets the position of the clone to (i,0,0)
+            graphPoint.localPosition = Vector3.right * i / 5f; // sets the position of the clone to (i,0,0) w.r.t the current scale
+            graphPoint.localScale = Vector3.one / 5f; // reducing the scale of the clones to 1/5th of the original
         }
         Destroy(GameObject.Find("Cube")); // destroys the original gameObject
     }
