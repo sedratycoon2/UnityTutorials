ec30105d8dcb9bd2278e51ddd31130d35c7645e0
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 15:51:36 -0300

    Created an array to store newly created clone's transform.

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -8,6 +8,7 @@ public class GraphGenerator : MonoBehaviour {
     // range to create more cubes, which can be adjusted via slider
     [Range(10,100)]
     public int graphResolution = 10;
+    Transform [] graphPoints;
 
     private void Awake()
     {
@@ -15,11 +16,13 @@ public class GraphGenerator : MonoBehaviour {
         Vector3 graphScale = Vector3.one * graphStep;
         Vector3 graphPosition;
         graphPosition.z = 0f;
+        graphPoints = new Transform[graphResolution];
         for (int i=0; i < graphResolution; i++)
         {
             Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
+            graphPoints[i] = graphPoint; // storing the newly created clone's transform
             graphPosition.x = (i + 0.5f) * graphStep - 1f; // sets the x-position of the clone in the range (-1,1)
-            graphPosition.y = Mathf.Pow(graphPosition.x, 2); // sets the y-position of the clone in the range (-1,1) 
+            graphPosition.y = Mathf.Pow(graphPosition.x, 3); // sets the y-position of the clone in the range (-1,1) 
             graphPoint.localPosition = graphPosition;
             graphPoint.localScale = graphScale; // reducing the scale of the clones to bring to (-1,1) domain
             graphPoint.SetParent(transform, false);
