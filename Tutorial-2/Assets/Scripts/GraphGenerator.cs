e40c4bc81a99aab56e19d7b14b0efde95cd02a92
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 13:07:53 -0300

    Set the y-position of the graph to represent y = f(x)

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -10,12 +10,12 @@ public class GraphGenerator : MonoBehaviour {
     {
         Vector3 graphScale = Vector3.one / 5f;
         Vector3 graphPosition;
-        graphPosition.y = 0;
         graphPosition.z = 0;
         for (int i=0; i < 10; i++)
         {
             Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
-            graphPosition.x = (i + 0.5f) / 5f - 1f; // sets the position of the clone to (i,0,0) in the range (-1,1)
+            graphPosition.x = (i + 0.5f) / 5f - 1f; // sets the x-position of the clone in the range (-1,1)
+            graphPosition.y = Mathf.Pow(graphPosition.x, 2); // sets the y-position of the clone in the range (-1,1) 
             graphPoint.localPosition = graphPosition;
             graphPoint.localScale = graphScale; // reducing the scale of the clones to bring to (-1,1) domain
         }
