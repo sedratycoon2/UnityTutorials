a7a7105051e62224816f04a063277c836b8dd5cf
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-20 12:49:25 -0300

    Changed the domain from [0,2] to [-1,1]

--- Tutorial-2/Assets/Scripts/GraphGenerator.cs
+++ Tutorial-2/Assets/Scripts/GraphGenerator.cs
@@ -11,8 +11,8 @@ public class GraphGenerator : MonoBehaviour {
         for (int i=0; i < 10; i++)
         {
             Transform graphPoint = Instantiate(graphPointPrefab); // creates a clone of the cube
-            graphPoint.localPosition = Vector3.right * i / 5f; // sets the position of the clone to (i,0,0) w.r.t the current scale
-            graphPoint.localScale = Vector3.one / 5f; // reducing the scale of the clones to 1/5th of the original
+            graphPoint.localPosition = Vector3.right * (i / 5f - 1f); // sets the position of the clone to (i,0,0) in the range [-1,1]
+            graphPoint.localScale = Vector3.one / 5f; // reducing the scale of the clones to bring to [-1,1] domain
         }
         Destroy(GameObject.Find("Cube")); // destroys the original gameObject
     }
