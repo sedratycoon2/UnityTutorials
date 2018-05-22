b2f79a3efb21da22868e7548be9346e2d10e51e0
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-22 11:17:53 -0300

    Added variables to control no of fractal child being created and set the max depth to 4.

--- Tutorial-4/Assets/Scripts/Fractal.cs
+++ Tutorial-4/Assets/Scripts/Fractal.cs
@@ -6,10 +6,20 @@ public class Fractal : MonoBehaviour {
     public Mesh fractalMesh;
     public Material newFractalMaterial;
 
+    // the following variables control the amount of frctal children being created
+    public int maxDepth;
+    private int depth;
+
 	void Start ()
     {
         // adds new mesh and material to the attached gameObject
         gameObject.AddComponent<MeshFilter>().mesh = fractalMesh;
         gameObject.AddComponent<MeshRenderer>().material = newFractalMaterial;
+        
+        // only allow creation of children if condition satisfies
+        if (depth < maxDepth)
+        {
+            new GameObject("Fractal Child").AddComponent<Fractal>();
+        }
 	}
 }
