7415bec5086ad0f0e9ad1bbd03227ae3a1816372
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-22 12:21:45 -0300

    Added static array variables for child directions and orientations to enhance code readability.

--- Tutorial-4/Assets/Scripts/Fractal.cs
+++ Tutorial-4/Assets/Scripts/Fractal.cs
@@ -6,13 +6,25 @@ public class Fractal : MonoBehaviour {
     public Mesh fractalMesh;
     public Material newFractalMaterial;
 
-    // the following two variables control the amount of fractal children being created
+    // variables to control the amount and scale of fractal children being created
     public int maxDepth;
     private int depth;
-
     public float fractalChildScale;
 
-	void Start ()
+    // variables to enhance readability of child creation code
+    private static Vector3[] fractalChildDirections = {
+        Vector3.up,
+        Vector3.right,
+        Vector3.left
+    };
+
+    private static Quaternion[] fractalChildOrientations = {
+        Quaternion.identity,
+        Quaternion.Euler(0, 0, -90f),
+        Quaternion.Euler(0, 0, 90f)
+    };
+
+    void Start ()
     {
         // adds new mesh and material to the attached gameObject
         gameObject.AddComponent<MeshFilter>().mesh = fractalMesh;
@@ -26,7 +38,7 @@ public class Fractal : MonoBehaviour {
 	}
 
     // method to create new fractal child with similar mesh, material and maxDepth settings as parent
-    private void InitializeChild (Fractal parent, Vector3 direction, Quaternion orientation)
+    private void InitializeChild (Fractal parent, int childIndex)
     {
         fractalMesh = parent.fractalMesh;
         newFractalMaterial = parent.newFractalMaterial;
@@ -36,20 +48,19 @@ public class Fractal : MonoBehaviour {
         transform.parent = parent.transform; // sets parent as the root
         transform.localScale = Vector3.one * fractalChildScale; // sets the scale of the child
         // sets the position of the child such that it's placed above the parent while still touching it
-        transform.localPosition = direction * (0.5f + 0.5f * fractalChildScale);
+        transform.localPosition = fractalChildDirections[childIndex] 
+            * (0.5f + 0.5f * fractalChildScale);
+        transform.localRotation = fractalChildOrientations[childIndex];
     }
 
     // coroutine which pauses for 0.5 seconds before creating the child fractal
     private IEnumerator CreateFractalChildren()
     {
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").
-                AddComponent<Fractal>().InitializeChild(this, Vector3.up, Quaternion.identity);
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").
-                AddComponent<Fractal>().InitializeChild(this, Vector3.right, Quaternion.Euler(0, 0, -90f));
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").
-                AddComponent<Fractal>().InitializeChild(this, Vector3.left, Quaternion.Euler(0, 0, 90f));
+        for (int i=0; i < fractalChildDirections.Length; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            new GameObject("Fractal Child").
+                    AddComponent<Fractal>().InitializeChild(this, i);
+        }
     }
 }
