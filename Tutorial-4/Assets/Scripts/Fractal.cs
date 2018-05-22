862925f71ca0c0561e2ed7b2e9155ceb82cb35d7
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-22 11:41:21 -0300

    Added a variable to control scale of the fractal child and set it to 0.5. Also, modified the method to place the fractal child right above the parent while still touching it.

--- Tutorial-4/Assets/Scripts/Fractal.cs
+++ Tutorial-4/Assets/Scripts/Fractal.cs
@@ -6,9 +6,11 @@ public class Fractal : MonoBehaviour {
     public Mesh fractalMesh;
     public Material newFractalMaterial;
 
-    // the following variables control the amount of frctal children being created
+    // the following two variables control the amount of fractal children being created
     public int maxDepth;
     private int depth;
+
+    public float fractalChildScale;
 
 	void Start ()
     {
@@ -29,8 +31,13 @@ public class Fractal : MonoBehaviour {
     {
         fractalMesh = parent.fractalMesh;
         newFractalMaterial = parent.newFractalMaterial;
+        fractalChildScale = parent.fractalChildScale;
         maxDepth = parent.maxDepth;
         depth = parent.depth + 1;
         transform.parent = parent.transform; // sets parent as the root
+        transform.localScale = Vector3.one * fractalChildScale; // sets the scale of the child
+        // sets the position of the child such that it's placed above the parent
+        // while still touching it
+        transform.localPosition = Vector3.up * (0.5f + 0.5f * fractalChildScale);
     }
 }
