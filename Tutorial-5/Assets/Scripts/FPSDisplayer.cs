5d4f0a82a824692765730dd476d7d476e01cc723
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-28 14:26:48 -0300

    Created FPSDisplayer.cs to display the FPS on the panel.

---
+++ Tutorial-5/Assets/Scripts/FPSDisplayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSCounter))]
+public class FPSDisplayer : MonoBehaviour {
+
+    public Text fpsLabel;
+
+    FPSCounter fpsCounter;
+
+    private void Awake()
+    {
+        fpsCounter = GetComponent<FPSCounter>();
+    }
+
+    private void Update()
+    {
+        fpsLabel.text = fpsCounter.FPS.ToString;
+    }
+}
