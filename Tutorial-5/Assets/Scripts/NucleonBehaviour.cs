5f702081bdec59b7e94151a6976072d68f91057b
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-28 12:01:58 -0300

    Created NucleonBehaviour script to control the nucleon's behaviour.

---
+++ Tutorial-5/Assets/Scripts/NucleonBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class NucleonBehaviour : MonoBehaviour {
+
+    public float attractionForce;
+
+    Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+}
