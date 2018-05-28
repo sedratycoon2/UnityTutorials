4fc71a7c45e58a1ab349767e2d67d8fdec4bb635
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-28 17:20:09 -0300

    Created a private struct to add color to the displayed fps for various fps achieved.

--- Tutorial-5/Assets/Scripts/FPSDisplayer.cs
+++ Tutorial-5/Assets/Scripts/FPSDisplayer.cs
@@ -20,6 +20,16 @@ public class FPSDisplayer : MonoBehaviour {
         "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
         "90", "91", "92", "93", "94", "95", "96", "97", "98", "99" };
 
+    [System.Serializable]
+    private struct FPSColor
+    {
+        public Color color;
+        public int minimumFPS;
+    }
+
+    [SerializeField]
+    private FPSColor[] coloring;
+
     private void Awake()
     {
         fpsCounter = GetComponent<FPSCounter>();
