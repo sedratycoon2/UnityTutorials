78755ce7884d055f4dd3790e4a2e966746522c0e
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-28 12:45:41 -0300

    Created a method to spawn nucleons using random prefab and random position. The nucleon is spawned by if the time since last spawn is greater or equals time between spawns.

--- Tutorial-5/Assets/Scripts/NucleonSpawner.cs
+++ Tutorial-5/Assets/Scripts/NucleonSpawner.cs
@@ -6,5 +6,25 @@ public class NucleonSpawner : MonoBehaviour {
 
     public float spawnDistance;
 
-    public Nucleon[] nucleonPrefabs; 
+    public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn = 0f;
+
+    private void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    // method to instantiate random prefab at a random position
+    void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
 }
