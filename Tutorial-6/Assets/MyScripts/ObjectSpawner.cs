9131d9b9da9c207fb158dbd18fd3fbf34faddde6
sedratycoon2 <sedratycoon2@gmail.com>
2018-05-30 12:25:15 -0300

    Created ObjectSpawner.cs to instantiate a random prefab.

---
+++ Tutorial-6/Assets/MyScripts/ObjectSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    public float timeBetweenSpawns;
+
+    public Stuff[] objectPrefabs;
+
+    float timeSinceLastSpawn;
+
+    private void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if(timeSinceLastSpawn >= timeBetweenSpawns) {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnPrefabs();
+        }
+    }
+
+    // method to spawn different prefabs based on the selection made in the editor
+    void SpawnPrefabs()
+    {
+        Stuff prefab = objectPrefabs[Random.Range(0, objectPrefabs.Length)];
+        Stuff spawn = Instantiate<Stuff>(prefab);
+        spawn.transform.localPosition = transform.position;
+    }
+}
