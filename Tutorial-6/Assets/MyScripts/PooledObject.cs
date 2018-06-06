479d84deacba35fa21a4a8155f9487a7d67dab33
sedratycoon2 <sedratycoon2@gmail.com>
2018-06-06 10:35:01 -0300

    Created PooledObject.cs to to reuse gameObjects when needed by placing them in the buffer and deactivating them. Also, destroys gameObjects if no pool is assigned to it.

---
+++ Tutorial-6/Assets/MyScripts/PooledObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PooledObject : MonoBehaviour {
+
+    public ObjectPool Pool { get; set; }
+
+    public void ReturnToPool()
+    {
+        if (Pool) {
+            Pool.AddObject(this);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+}
