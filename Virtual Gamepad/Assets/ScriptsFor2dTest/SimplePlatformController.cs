1c0af9184353c45c8323433f226082c8b2d36965
sedratycoon2 <sedratycoon2@gmail.com>
2018-06-11 12:15:02 -0300

    Practice platformer and joystick tutorials.

---
+++ Virtual Gamepad/Assets/ScriptsFor2dTest/SimplePlatformController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class SimplePlatformController : MonoBehaviour {
+
+    [HideInInspector]
+    public bool facingRight = true;
+    [HideInInspector]
+    public bool jump = true;
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+
+    private bool grounded = false;
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 
+            1 << LayerMask.NameToLayer("Ground"));
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(horizontal));
+        Vector2 currentVelocity = rb2d.velocity;
+        
+        if (horizontal * currentVelocity.x < maxSpeed)
+        {
+            rb2d.AddForce(Vector2.right * horizontal * moveForce);
+        }
+
+        if (Mathf.Abs(currentVelocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(currentVelocity.x) * maxSpeed, currentVelocity.y);
+
+        if (horizontal > 0 && !facingRight)
+            Flip();
+        else if (horizontal < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            anim.SetTrigger("Jump");
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Virtual Gamepad/Assets/ScriptsForVirtualJoystick/VirtualJoystick.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
+
+    private Image bgImage;
+    private Image joystickImage;
+    private Vector2 inputVector;
+
+    private void Start()
+    {
+        bgImage = GetComponent<Image>();
+        joystickImage = transform.GetChild(0).GetComponent<Image>(); //gets image of the child
+    }
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            bgImage.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / bgImage.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImage.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector2(pos.x * 2 - 1, pos.y * 2 - 1);
+            // helps limit values to the extent of the circle
+            inputVector = (inputVector.magnitude > 1) ? inputVector.normalized : inputVector;
+
+            // moving the joystick image
+            joystickImage.rectTransform.anchoredPosition = new Vector2(
+                inputVector.x * (bgImage.rectTransform.sizeDelta.x/2.5f), 
+                inputVector.y * (bgImage.rectTransform.sizeDelta.y/2.5f));
+        }
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        inputVector = Vector2.zero;
+        joystickImage.rectTransform.anchoredPosition = Vector2.zero;
+    }
+
+	public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+}
